b446ef4c8df6e9adc1b8f41440ac3fb20616fd63
vbhalter <vbhalter@uark.edu>
2021-06-30 13:02:29 -0500

    Add project files.

---
+++ Snake/Snake.py
@@ -0,0 +1,65 @@
+from graphics import *
+import time
+from pynput import keyboard
+
+
+snake = Rectangle(Point(380,395), Point(420,405))
+win = GraphWin("My Circle", 800, 800)
+runGame = True
+direction = 'right'
+
+def on_press(key):
+	global runGame
+	global direction
+
+	#Set movement direction and check for escape key
+	if key.char == 'd':
+		direction = 'right'
+	elif key.char == 'a':
+		direction = 'left'
+	elif key.char == 'w':
+		direction = 'up'
+	elif key.char == 's':
+		direction = 'down'
+	elif key == keyboard.Key.esc:
+		runGame = False
+		return False
+
+
+listener = keyboard.Listener(on_press=on_press)
+
+
+def main():
+	initialize()
+
+	runGame()
+
+
+
+
+
+def initialize():
+	#Draw snake
+	snake.setFill("green")
+	snake.setOutline("green")
+	snake.draw(win)
+
+
+def runGame():
+	listener.start()
+
+	while(runGame):
+		moveSnake(direction)
+
+def moveSnake(direction):
+	time.sleep(.001)
+	if direction == 'up':
+		snake.move(0, -2)
+	elif direction == 'down':
+		snake.move(0, 2)
+	elif direction == 'left':
+		snake.move(-2, 0)
+	elif direction == 'right':
+		snake.move(2, 0)
+
+main()
