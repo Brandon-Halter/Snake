7327e42a362a33fd54f2b48b06dd5501a97d8e89
Brandon-Halter <Bragdon501@gmail.com>
2021-07-01 14:03:35 -0500

    Added comments and snakeSegment file. The snakeSegment file contains the unfinished snakeSegment class.

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -1,56 +1,55 @@
 from graphics import *
-import time
 from pynput import keyboard
+import time
+import snakeSegment
 
+#Create global variables
 test = "test"
 snake = Rectangle(Point(380,395), Point(420,405))
 win = GraphWin("My Circle", 800, 800)
 runGame = True
 direction = 'right'
 
+#Define behavior for keyboard listener
 def on_press(key):
 	global runGame
 	global direction
 
-	#Set movement direction and check for escape key
-	if key.char == 'd':
-		direction = 'right'
-	elif key.char == 'a':
-		direction = 'left'
-	elif key.char == 'w':
-		direction = 'up'
-	elif key.char == 's':
-		direction = 'down'
-	elif key == keyboard.Key.esc:
-		runGame = False
-		return False
+	try:
+		if key == keyboard.Key.esc:
+			runGame = False
+			return False
+		elif key.char == 'd':
+			direction = 'right'
+		elif key.char == 'a':
+			direction = 'left'
+		elif key.char == 'w':
+			direction = 'up'
+		elif key.char == 's':
+			direction = 'down'
+
+	except AttributeError:
+		pass
+
 
 
 listener = keyboard.Listener(on_press=on_press)
 
 
-def main():
-	initialize()
-
-	runGame()
-
-
-
-
-
+#Draw initial window
 def initialize():
-	#Draw snake
 	snake.setFill("green")
 	snake.setOutline("green")
 	snake.draw(win)
 
-
+#Run the game
 def runGame():
 	listener.start()
 
 	while(runGame):
 		moveSnake(direction)
 
+#Make the snake respond to movement inputs
 def moveSnake(direction):
 	time.sleep(.001)
 	if direction == 'up':
@@ -61,5 +60,11 @@ def moveSnake(direction):
 		snake.move(-2, 0)
 	elif direction == 'right':
 		snake.move(2, 0)
+
+
+def main():
+	initialize()
+
+	runGame()
 
 main()
---
+++ Snake/snakeSegment.py
@@ -0,0 +1,26 @@
+class snakeSegment:
+    segment = Rectangle(Point(0,0), Point(0,0))
+    segP1 = Point(0,0)
+    segP2 = Point(0,0)
+    segDirection = "right"
+    segLength = 0
+
+    def __init__(self, p1, p2, direction, length, segment):
+        self.segP1 = p1
+        self.segP2 = p2
+        self.segDirection = direction
+        self.segLength = length
+
+        self.segment = Rectangle(p1, p2)
+        self.segment.setFill("green")
+        self.segment.setOutline("green")
+
+    def shrinkSegment():
+        if segDirection == "right":
+            segP1 = Point(segP1.getX() + 2, segP1.getY())
+        elif segDirection == "left":
+            segP2 = Point(segP2.getX() - 2, segP2.getY())
+        elif segDirection == "up":
+            segP1 = Point(segP1.getX(), segP1.getY() + 2)
+        elif segDirection == "right":
+            segP2 = Point(segP2.getX(), segP2.getY() - 2)
