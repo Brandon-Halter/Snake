91bfa8fa5b74ebbbe8ae5dc8004e1f9c5ee1ea9b
Brandon-Halter <Bragdon501@gmail.com>
2021-07-20 13:28:37 -0500

    Continued work on snake movement.

--- Snake/Snake.py
+++ Snake/Snake.py
@@ -6,7 +6,7 @@ from snakeSegment import snakeSegment
 #Create global variables
 initialSegment = snakeSegment(Point(380,395), Point(420,405), "right", 40)
 segments = [initialSegment]
-win = GraphWin("My Circle", 800, 800)
+win = GraphWin("gameWindow", 800, 800, autoflush=False)
 runGame = True
 direction = 'right'
 
@@ -20,16 +20,34 @@ def on_press(key):
 			runGame = False
 			return False
 		elif key.char == 'd':
+			segments.append(snakeSegment(Point(200, 200), Point(220,240), direction, 40))
 			direction = 'right'
+			segments[0].segDirection = direction
 		elif key.char == 'a':
+			segments.append(snakeSegment(Point(200, 200), Point(220,240), direction, 40))
 			direction = 'left'
+			segments[0].segDirection = direction
 		elif key.char == 'w':
+			segments.append(snakeSegment(Point(200, 200), Point(240,220), direction, 40))
 			direction = 'up'
+			segments[0].segDirection = direction
 		elif key.char == 's':
+			segments.append(snakeSegment(Point(200, 200), Point(240,220), direction, 40))
 			direction = 'down'
+			segments[0].segDirection = direction
 
-	except AttributeError:
+	except:
 		pass
+
+def clearWindow(win):
+	for item in win.items[:]:
+		item.undraw()
+
+
+def redrawWindow(win, segments):
+	for segment in segments:
+		segment.segment.draw(win)
+	win.update()
 
 
 
@@ -39,6 +57,7 @@ listener = keyboard.Listener(on_press=on_press)
 #Draw initial window
 def initialize():
 	(initialSegment.segment).draw(win)
+	win.update()
 
 #Run the game
 def runGame():
@@ -46,19 +65,28 @@ def runGame():
 
 	while(runGame):
 		for s in segments:
-			moveSnake(direction, s.segment)
+			moveSnake(direction)
 
 #Make the snake respond to movement inputs
-def moveSnake(direction, snake):
+def moveSnake(direction):
 	time.sleep(.001)
-	if direction == 'up':
-		snake.move(0, -2)
-	elif direction == 'down':
-		snake.move(0, 2)
-	elif direction == 'left':
-		snake.move(-2, 0)
-	elif direction == 'right':
-		snake.move(2, 0)
+
+	#Remove end of snake
+	lastSegment = segments[len(segments) - 1]
+	lastSegment.shrinkSegment()
+
+	#Check if last segment is completely gone
+	if lastSegment.segLength <= 0:
+		segments.pop(len(segments) - 1)
+
+	#Add front of snake
+	firstSegment = segments[0]
+	firstSegment.growSegment(direction)
+
+
+
+	clearWindow(win)
+	redrawWindow(win, segments)
 
 
 def main():
--- Snake/snakeSegment.py
+++ Snake/snakeSegment.py
@@ -7,6 +7,8 @@ class snakeSegment:
     segDirection = "right"
     segLength = 0
 
+#=================================================================================================
+
     def __init__(self, p1, p2, direction, length):
         self.segP1 = p1
         self.segP2 = p2
@@ -17,12 +19,88 @@ class snakeSegment:
         self.segment.setFill("green")
         self.segment.setOutline("green")
 
-    def shrinkSegment():
-        if segDirection == "right":
-            segP1 = Point(segP1.getX() + 2, segP1.getY())
-        elif segDirection == "left":
-            segP2 = Point(segP2.getX() - 2, segP2.getY())
-        elif segDirection == "up":
-            segP1 = Point(segP1.getX(), segP1.getY() + 2)
-        elif segDirection == "right":
-            segP2 = Point(segP2.getX(), segP2.getY() - 2)
+#=================================================================================================
+
+    def shrinkSegment(self):
+        #Change coordinates
+        if self.segDirection == "right":
+            #Check to see if P1 or P2 is trailing edge
+            if self.segP1.getX() < self.segP2.getX():
+                self.segP1 = Point(self.segP1.getX() + 2, self.segP1.getY()) 
+            else:
+                self.segP2 = Point(self.segP2.getX() + 2, self.segP2.getY()) 
+        elif self.segDirection == "left":
+            #Check to see if P1 or P2 is trailing edge
+            if self.segP1.getX() < self.segP2.getX():
+                self.segP2 = Point(self.segP2.getX() - 2, self.segP2.getY())
+            else:
+                self.segP1 = Point(self.segP1.getX() - 2, self.segP1.getY())
+        elif self.segDirection == "up":
+            #Check to see if P1 or P2 is trailing edge
+            if self.segP1.getY() > self.segP2.getY():
+                self.segP1 = Point(self.segP1.getX(), self.segP1.getY() - 2)
+            else:
+                self.segP2 = Point(self.segP2.getX(), self.segP2.getY() - 2)
+        elif self.segDirection == "down":
+            #Check to see if P1 or P2 is trailing edge
+            if self.segP1.getY() > self.segP2.getY():
+                self.segP2 = Point(self.segP2.getX(), self.segP2.getY() + 2)
+            else:
+                self.segP1 = Point(self.segP1.getX(), self.segP1.getY() + 2)
+        self.segLength = self.segLength - 2
+
+        #Update segment with new coordinates
+        self.segment = Rectangle(self.segP1, self.segP2)
+        self.segment.setFill("green")
+        self.segment.setOutline("green")
+
+#=================================================================================================
+
+    def growSegment(self, direction):
+        #Change coordinates
+        if direction == "right":
+            #Check to see if P1 or P2 is leading edge
+            if self.segP1.getX() > self.segP2.getX():
+                self.segP1 = Point(self.segP1.getX() + 2, self.segP1.getY())
+            else:
+                self.segP2 = Point(self.segP2.getX() + 2, self.segP2.getY())
+        elif direction == "left":
+            #Check to see if P1 or P2 is leading edge
+            if self.segP1.getX() < self.segP2.getX():
+                self.segP1 = Point(self.segP1.getX() - 2, self.segP1.getY())
+            else:
+                self.segP2 = Point(self.segP2.getX() - 2, self.segP2.getY())
+        elif direction == "up":
+            #Check to see if P1 or P2 is leading edge
+            if self.segP1.getY() < self.segP2.getY():
+                self.segP1 = Point(self.segP1.getX(), self.segP1.getY() - 2)
+            else:
+                self.segP2 = Point(self.segP2.getX(), self.segP2.getY() - 2)
+        elif direction == "down":
+            #Check to see if P1 or P2 is leading edge
+            if self.segP1.getY() < self.segP2.getY():
+                self.segP2 = Point(self.segP2.getX(), self.segP2.getY() + 2)
+            else:
+                self.segP1 = Point(self.segP1.getX(), self.segP1.getY() + 2)
+
+        self.segLength = self.segLength + 2
+
+        #Update segment with new coordinates
+        self.segment = Rectangle(self.segP1, self.segP2)
+        self.segment.setFill("green")
+        self.segment.setOutline("green")
+
+#=================================================================================================
+
+    def turnSegment(self):
+        if self.direction == "right":
+            #Calculate coordinates after turn
+            currentLocation = self.segment.getCenter()
+            self.segP1 = Point(currentLocation.getX() - 5, currentLocation.getY() + self.height/2)
+            self.segP2
+        elif self.direction == "left":
+            print(test)
+        elif self.direction == "up":
+            print(test)
+        else:
+            print(test)
